12d3b5081749db9d6abc582f0d371ed57780aead
GallanSJK <gallansteffan99@gmail.com>
2023-10-27 20:46:57 +0700

    create api get,delete,update,add

---
+++ backend/api/todo.controller.js
@@ -0,0 +1,76 @@
+const models = require('./../models/index')
+const todo = models.data_todo
+
+const express = require('express')
+const app = express()
+
+app.get("/", async(req, res) =>{
+    await todo.findAll({
+        attributes: ["id", "name", "status"]
+    })
+    .then((result) => {
+        res.json({
+            data: result
+        })
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
+app.post("/", async(req, res) =>{
+    const {
+        name,
+        status
+    } = req.body
+    const data = {
+        name: name,
+        status: status
+    }
+    await todo.create(data)
+    .then(result => {
+        res.json({
+            data: data
+        })
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
+app.put("/:id", async(req, res) =>{
+    const {id} = req.params
+    const {name, status} = req.body
+    const data = {
+        name: name,
+        status: status
+    }
+    await todo.update(data, {
+        where: {
+            id: id
+        }
+    })
+    .then(result => {
+        res.json({
+            data: data
+        })
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
+app.delete("/:id", async(req, res) =>{
+    const {id} = req.params
+    await todo.destroy({
+        where: {
+            id: id
+        }
+    })
+    .then(result => {
+        res.json({
+            message: "Data was deleted"
+        })
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
+module.exports = app
---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express())
+app.use(bodyParser.json())
+app.use(cors())
+
+const Todo = require("./api/todo.controller")
+app.use('/api/todo', Todo)
+
+
+app.listen(8080, (()=> console.log("Server run on port 8080")))
